5ebab315b386708e51d781b6de6c9743e594fcb4
Homer Liu <kingliu06@gmail.com>
2023-01-15 16:53:23 +0800

    Add New File

--- age.py
+++ age.py
@@ -1,12 +1,21 @@
 driving = input ('請問你有沒有開過車:')
 age = input ('請問您今年幾歲:')
 
-age = int (age)
+age = int (age)                 #casting
 
 if driving == '有':
 	if age >= 18:
 		print ('沒問題，您是可以開車的年齡')
-	else:
+	elif age <18:
 		print ('奇怪，您不是可以駕駛的年紀')
+	else:
+		print ('請以整數作答')
+elif driving == '沒有':
+	if age >=18:
+		print ('可以快去考駕照了喔')
+	elif age < 18:
+		print ('小事啦，當個守法小公民')
+	else:
+		print ('請以整數作答')
 else:
-	print ('小事啦，當個守法小公民')
+	print ('請以「有」或「沒有」作答')
